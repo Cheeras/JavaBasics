bff071c6bcf291c9562a7301c651ecc602efa4a2
Cheeras <shankarcheerala@gmail.com>
2025-06-03 11:59:01 +0530

    Nested loops and time complexity

---
+++ JavaBasics/src/com/basics/TimeComplexity.java
@@ -0,0 +1,30 @@
+package com.basics;
+
+public class TimeComplexity {
+
+	public static void main(String[] args) {
+		
+		//What is mean by time complexity
+		//Big oh() - notation
+		//is only for java or other laungages as well
+		//Yes it is applicable for all the Laungages java, .net ,javascript...etc
+		
+		//1
+		int i = 1;
+		System.out.println(i);
+		//time complexity of the above code 
+		//O(1)--constant time
+		
+		//2
+		for(int p=1;p<=10;p++) {
+			System.out.println(p);
+		}
+		//	
+		
+		
+		
+		
+
+	}
+
+}
