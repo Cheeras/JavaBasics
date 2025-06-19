56b9f5afa71b2c35e4f1f29f2f9e181c0e06d5d9
Cheeras <shankarcheerala@gmail.com>
2025-06-19 19:21:22 +0530

    Switch case Usage: Days of the week

--- JavaBasics/src/com/basics/SwitchCaseStatementJava8.java
+++ JavaBasics/src/com/basics/SwitchCaseStatementJava8.java
@@ -6,14 +6,27 @@ public class SwitchCaseStatementJava8 {
 	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter fruit name:");
-		String fruit =  in.next();
-		switch(fruit) {
-		case "Mango" -> System.out.println("King of fruit");
-		case "Apple" -> System.out.println("A sweet red fruit");
-		case "Orange" -> System.out.println("Round fruit");
-		case "Grapes" -> System.out.println("Small fruit");
-		default -> System.out.println("Please enter valid fruit name");
+//		System.out.println("Enter fruit name:");
+		//String fruit =  in.next();
+//		switch(fruit) {
+//		case "Mango" -> System.out.println("King of fruit");
+//		case "Apple" -> System.out.println("A sweet red fruit");
+//		case "Orange" -> System.out.println("Round fruit");
+//		case "Grapes" -> System.out.println("Small fruit");
+//		default -> System.out.println("Please enter valid fruit name");
+//		}
+		System.out.println("Days of a week");
+		int day = in.nextInt();
+		
+		switch(day) {
+		case 1 -> System.out.println("Sunday");
+		case 2 -> System.out.println("Monday");
+		case 3 -> System.out.println("Tuesday");
+		case 4 -> System.out.println("Wednesday");
+		case 5 -> System.out.println("Thursday");
+		case 6 -> System.out.println("Friday");
+		case 7 -> System.out.println("Saturday");
+		default -> System.out.println("Invalid day");
 		}
 		in.close();
 	}
