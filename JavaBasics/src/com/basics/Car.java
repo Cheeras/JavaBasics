0ef9364becdecc121e619ea6f5c251749e9e68d4
Cheeras <shankarcheerala@gmail.com>
2025-06-22 13:27:35 +0530

    JVM Memory architecture Diagram

--- JavaBasics/src/com/basics/Car.java
+++ JavaBasics/src/com/basics/Car.java
@@ -5,7 +5,7 @@ public class Car {
 	String name;
 	int price;
 	String modelNumber;
-	int wheels;
+	static int wheels=4;
 
 	public static void main(String[] args) {
 
@@ -13,26 +13,36 @@ public class Car {
 		c1.name = "BMW";
 		c1.price = 60;
 		c1.modelNumber = "x3";
-		c1.wheels = 4;
 		
 		Car c2 = new Car();
 		c2.name = "Audi";
 		c2.price = 65;
 		c2.modelNumber = "q3";
-		c2.wheels = 4;
 		
 		Car c3 = new Car();
-		c3.name = "Audi";
-		c3.price = 65;
+		c3.name = "Nexon";
+		c3.price = 35;
 		c3.modelNumber = "q3";
-		c3.wheels = 4;
 
 		Car c4 = new Car();
 		c4.name = "Tata Nexon";
 		c4.price = 25;
 		c4.modelNumber = "ex";
-		c4.wheels = 4;
 		
+		//how to access the static variable
+		//1. using the class name
+		System.out.println(Car.wheels);
+		//2. Using directly
+		System.out.println(wheels);
+		//3. Can i access static variable using the Object ref name?
+		System.out.println(c1.wheels);//Warning: Static field should be accessed
+		//in static way
+		System.out.println(c2.name);//to access non static variable we should 
+		//call using the ref object
+		//how to access the non static variable: Using the object reference
+		System.out.println(c1.name +" "+c1.price+" "+c1.modelNumber);
+		System.out.println(c2.name +" "+c2.price+" "+c2.modelNumber);
+		System.out.println(c3.name +" "+c3.price+" "+c3.modelNumber);
 
 	}
 
