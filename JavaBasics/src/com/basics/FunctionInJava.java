0a1321ac502136253c0374e09d890d635d089820
Cheeras <shankarcheerala@gmail.com>
2025-06-24 23:40:36 +0530

    Functions in java

---
+++ JavaBasics/src/com/basics/FunctionInJava.java
@@ -0,0 +1,21 @@
+package com.basics;
+
+public class FunctionInJava {
+	//function / method in java
+	//it will give reusability of the code
+	//no input -- no return
+	//input -- return
+	//input -- no return
+	//no input - return
+	
+	//functions are independent to each other
+	//functions are parallel to each other
+	//  	
+	
+	
+	
+	public static void main(String[] args) {
+
+	}
+
+}
