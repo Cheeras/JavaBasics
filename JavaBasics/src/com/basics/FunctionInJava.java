652aa5b1f2277c55680240f6fc25189d9d04b95f
Cheeras <shankarcheerala@gmail.com>
2025-06-25 23:33:59 +0530

    Functions with return type integer

--- JavaBasics/src/com/basics/FunctionInJava.java
+++ JavaBasics/src/com/basics/FunctionInJava.java
@@ -11,24 +11,35 @@ public class FunctionInJava {
 	//functions are independent to each other
 	//functions are parallel to each other
 	
-	//no input and no return:
+	//1.no input and no return:
 	//void -- no return, function does not return anything(no return keyword)
 	public void test() {
 		System.out.println("I am from the test method");
 	}
-	
+	//no input(no of parameter) and no return:
+	//zero parameter
 	public void calc() {
 		System.out.println("Calc method");
 		int a = 10;
 		int b = 20;
 		int c = a + b;
 		System.out.println(c);
-		
+	}
+	
+	//2.no input(no of parameters) with some return
+	//return type of this function is int
+	public int getNumber() {
+		System.out.println("getNumber");
+		int fee=100;
+		int tax=20;
+		int totalfee = fee+tax;
+		return totalfee;
 	}
 	public static void main(String[] args) {
 		FunctionInJava obj = new FunctionInJava();
 		obj.test();
 		obj.calc();
+		System.out.println(obj.getNumber());
 	
 	}
 
