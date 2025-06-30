4eb78cd065f38abc63cce25fe4c5b7c1fb2c5e4a
Cheeras <shankarcheerala@gmail.com>
2025-06-30 10:55:55 +0530

    Constructor memory diagram

--- JavaBasics/src/constructorConcept/Employee.java
+++ JavaBasics/src/constructorConcept/Employee.java
@@ -10,23 +10,39 @@ public class Employee {
 	String dob;
 	boolean isPerm;
 
-	//Constructor
+	//Constructor 
 	/*
 	 * Constructor name should be same as class name, i can not give any other name
 	 * is it looks like function? may be it is looks like function but it is not function
 	 * constructor can not return anything, even void as well
 	 * avoid writing the business logic in constructor
 	 * Main purpose of the constructor is 
-	 * 		Is to used initialize the class variables
+	 * 		Is to used initialize the class(instance) variables with the help of local variables
+	 * using this keyword
 	 * Constructor can be overloaded - Yes we can overload
 	 * */
 	
-	public Employee() {//0 param constructor//default constructor
+/*	public Employee() {//0 param constructor//default constructor
 		
+	}
+	
+	public Employee(int a) {
+		
+	}
+	
+	public Employee(double d) {
+		
+	}*/
+	
+	public Employee(String name, int age) {
+		//global = local
+		this.name = name;
+		this.age = age;
 	}
 	public static void main(String[] args) {
 
-			Employee e1 = new Employee();
+			Employee e1 = new Employee("Priya",30);
+			System.out.println(e1.name +" "+ e1.age);
 	}
 
 }
