92dfd9d176ed68f93e622dd04edbedea7da215dc
Cheeras <shankarcheerala@gmail.com>
2026-03-11 04:15:41 +0530

    Inheritance notes

---
+++ JavaBasics/src/com/selenium/basics/TableHandling.java
@@ -0,0 +1,10 @@
+package com.selenium.basics;
+
+public class TableHandling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//WebDriverManager.chromedriver().setUp();
+
+	}
+}
